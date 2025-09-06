76fa88d4e3e766f48844a77bc6a7527b51f60b35
ezashi <soma3070@icloud.com>
2025-09-06 17:33:52 +0900

    feat: 書籍管理機能の追加とユーザー認証の改善
    - Gemfileにbcryptを追加し、ユーザー認証機能を強化
    - 書籍の編集、追加、新規登録、詳細表示のためのビューを作成
    - 書籍一覧表示機能を追加し、検索機能を実装
    - スキーマにbooksテーブルとusersテーブルを追加し、関連付けを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_06_053912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "books", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.string "author"
+    t.string "publisher"
+    t.string "cover_image_url"
+    t.integer "rating"
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "title"], name: "index_books_on_user_id_and_title"
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  add_foreign_key "books", "users"
 end
